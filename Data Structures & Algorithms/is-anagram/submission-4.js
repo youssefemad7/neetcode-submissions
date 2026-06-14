2079d4e0aa388a1424e651635ec19caf1a8f715a
youssefemad7 <113909624+youssefemad7@users.noreply.github.com>
2026-06-14 16:11:19 +0300

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.js
@@ -0,0 +1,16 @@
+class Solution {
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false;
+
+        let sArr = s.split("").sort();
+        let tArr = t.split("").sort();
+
+        for (let i = 0; i < sArr.length; i++) {
+            if (sArr[i] !== tArr[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
